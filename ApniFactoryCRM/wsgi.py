ed29b4b6ec91b31a03f02532961f6852ef7f87a1
emtainfotech <info@emtainfotech.com>
2026-04-28 19:23:38 +0530

    Update wsgi.py to explicitly load .env and set path for Apache compatibility

--- ApniFactoryCRM/wsgi.py
+++ ApniFactoryCRM/wsgi.py
@@ -8,8 +8,17 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from pathlib import Path
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+# Add the project directory to the sys.path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
+# Explicitly load .env file for Apache/mod_wsgi
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ApniFactoryCRM.settings')
 
