6705a64c308c4f1bb265ea128c9c29a6fe11259c
emtainfotech <info@emtainfotech.com>
2026-07-21 14:13:02 +0530

    Whatsapp message error Catch Model and View

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from core.models import Customer
+from core.models import Customer, WhatsAppMessageStatus
 
 # Register your models here.
 
 admin.site.register(Customer)
+admin.site.register(WhatsAppMessageStatus)
